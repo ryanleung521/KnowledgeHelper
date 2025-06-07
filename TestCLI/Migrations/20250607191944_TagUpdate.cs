4f0d6e0a37b8aca411523d9b308bf490a9e1f0c6
Ryan <Ryan@DESKTOP-T1ATLVT>
2025-06-08 03:20:57 +0800

    Tag Database Update

---
+++ TestCLI/Migrations/20250607191944_TagUpdate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestCLI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Taggings",
+                columns: table => new
+                {
+                    EID = table.Column<int>(type: "int", nullable: false),
+                    TID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Taggings", x => new { x.EID, x.TID });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    TID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "0, 1"),
+                    TName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Taggings");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
